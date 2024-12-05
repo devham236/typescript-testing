e3b0bc45d8b3e079a831e3101280f8be23d4277f
Hamza Mirza <hamza665@hotmail.de>
2024-12-05 09:23:08 +0100

    deleted test file

--- src/Tests/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("should have hello world", () => {
-  render(<App />);
-  const text = screen.queryByText(/Hello World/i);
-  expect(text).toBeVisible();
-});
