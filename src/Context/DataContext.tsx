b976e255c48490c3cafaca53710d6a8df8440c07
Hamza Mirza <hamza665@hotmail.de>
2024-12-05 10:35:50 +0100

    set data to recipes

--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -1,8 +1,19 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 const DataContext = createContext();
 
 const DataContextProvider = ({ children }) => {
-  const [recipesData, setRecipesData] = useState([]);
+  const [recipesData, setRecipesData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/data.json");
+      const data = await res.json();
+
+      setRecipesData(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <DataContext.Provider value={{ recipesData, setRecipesData }}>
