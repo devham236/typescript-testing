8a7b028f52cb775a80cbb92e4ea2c07a3b1fc047
Hamza Mirza <hamza665@hotmail.de>
2024-12-05 09:49:24 +0100

    added links and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Recipes from "./Pages/Recipes";
+import Checkout from "./Pages/Checkout";
 
 const App = () => {
   return (
     <div>
-      <h1>hello world</h1>
+      <div className="links-container">
+        <Link to={"/"} style={{ marginRight: "1rem" }}>
+          <h2>Recipes</h2>
+        </Link>
+        <Link to={"/checkout"}>
+          <h2>Checkout</h2>
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Recipes />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </div>
   );
 };
